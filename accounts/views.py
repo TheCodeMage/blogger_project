dd3c14384076db62bc9d8b3d9061621824e9488c
TheCodeMage <mr.phyoekyawkyaw@gmail.com>
2025-04-04 15:53:14 +0630

    Second Commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+#Registration View
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) # Auto login after register
+            messages.success(request, "Registration Successful.")
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+#Login View
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+        
+#Logout View
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+ 
+
+        
+
